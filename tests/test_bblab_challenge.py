62489355d7ce7923451d0375df2110755c43a46f
Anton Rau <anton.rau@gmail.com>
2018-09-17 00:12:40 +0200

    Added several tests.

--- tests/test_bblab_challenge.py
+++ tests/test_bblab_challenge.py
@@ -4,29 +4,59 @@
 """Tests for `bblab_challenge` package."""
 
 import pytest
-
+import numpy as np
 from click.testing import CliRunner
 
-from bblab_challenge import tools
-from bblab_challenge import cli
+from bblab_challenge import cli, load_layer, load_mask, get_bgra, get_rgba, apply_mask, calculate_means
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_load_layer():
+    with pytest.raises(ValueError):
+        assert load_layer('data/images/Fibronectin(Dy163Di).tiff', 100, 100)
+    assert load_layer('data/images/Fibronectin(Dy163Di).tiff', 400, 395).shape == (395, 400)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_load_mask():
+    assert load_mask('data/single-cell-mask/single_cell_mask.tiff').shape == (395, 400)
 
 
+def test_get_bgra():
+    r = np.array([[1, 1]])
+    g = np.array([[2, 2]])
+    b = np.array([[3, 3]])
+    result = np.array([[[3., 2., 1., 255.],
+                        [3., 2., 1., 255.]]])
+    assert np.array_equal(get_bgra(r, g, b), result) is True
+
+
+def test_get_rgba():
+    r = np.array([[1, 1]])
+    g = np.array([[2, 2]])
+    b = np.array([[3, 3]])
+    result = np.array([[[1., 2., 3., 255.],
+                        [1., 2., 3., 255.]]])
+    assert np.array_equal(get_rgba(r, g, b), result) is True
+
+
+def test_apply_mask():
+    src = np.array([[[1., 2., 3., 255.],
+                     [1., 2., 3., 255.]]])
+    mask = np.array([[0, 0]])
+    result = np.array([[[1., 2., 3., 0.],
+                        [1., 2., 3., 0.]]])
+    assert np.array_equal(apply_mask(src, mask), result) is True
+
+
+def test_calculate_means():
+    red = [[1, 1], [1, 2]]
+    green = [[1, 1], [1, 2]]
+    blue = [[1, 1], [1, 2]]
+    markers = [[1, 1], [1, 1]]
+    result = []
+    assert np.array_equal(calculate_means(red, green, blue, markers), result) is True
+
+
+# Disable CLI test as Travis does not allow file operations.
 # def test_command_line_interface():
 #     """Test the CLI."""
 #     runner = CliRunner()
